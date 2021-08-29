7f011767a87ac607406adb9a29931a06e2acf230
Ludah <ghergo.ludovico@educ.di.unito.it>
2021-08-29 19:44:02 +0200

    Calcolo BoW LUX

--- Esercitazione_2/mapping.py
+++ Esercitazione_2/mapping.py
@@ -10,12 +10,15 @@ f = [[1582,"reference"],[2191,"turn_out"],[1670,"posing"],[15,"separating"],[232
 word = [] * 5
 word_sense = []
 FE_sense = []
+LU_sense = []
 for i in range(5):
     max_overlap = 0
     local = fn.frame(f[i][0])
     word1 = re.sub(r"[^a-zA-Z0-9 ]", "", local.definition).split()
     word2 = []
     FE_best_senses = []
+    LU_best_senses = []
+
     for elem in local.FE:
         val = local.FE[elem]
         word2 = word2 + (re.sub(r"[^a-zA-Z0-9 ]", "", val.definition).split())
@@ -56,6 +59,8 @@ for i in range(5):
         FE_best_senses.append(best_sense)
     FE_sense.append(FE_best_senses)
 
+
+
     best_sense = "None"
     for sense in wn.synsets(f[i][1]):
         signature = []
@@ -79,6 +84,43 @@ for i in range(5):
             max_overlap = overlap
             best_sense = sense
     word_sense.append(best_sense)
+
+    max_overlap = 0
+    for lex_u in local.lexUnit:
+        lex_u = lex_u[:len(lex_u)-2]
+        max_overlap_LU = 0
+        best_sense = "none"
+        senses_list = []
+        if wn.synsets(lex_u):
+            senses_list = wn.synsets(lex_u)
+        else:
+            lex_u = lex_u.lower().replace("_", " ")
+            lex_u = lex_u.split()
+            for single in lex_u:
+                senses_list = senses_list + wn.synsets(single)
+        for sense in senses_list:
+            elem_ctx = []
+            for example in sense.examples():
+                elem_ctx = elem_ctx + example.split()
+            for glos in sense.definition().split():
+                elem_ctx = elem_ctx + re.sub(r"[^a-zA-Z0-9]", "", glos).split()
+            for hypo in sense.hyponyms():
+                for example in hypo.examples():
+                    elem_ctx = elem_ctx + example.split()
+                for glos in hypo.definition().split():
+                    elem_ctx = elem_ctx + re.sub(r"[^a-zA-Z0-9]", "", glos).split()
+            for hyper in sense.hypernyms():
+                for example in hyper.examples():
+                    elem_ctx = elem_ctx + example.split()
+                for glos in hyper.definition().split():
+                    elem_ctx = elem_ctx + re.sub(r"[^a-zA-Z0-9]", "", glos).split()
+            overlap = len(set(elem_ctx).intersection(word[i]))
+            if overlap >= max_overlap_LU:
+                max_overlap_LU = overlap
+                best_sense = sense
+        LU_best_senses.append(best_sense)
+    LU_sense.append(LU_best_senses)
+
     pippo = 1
 
 print(ris)
