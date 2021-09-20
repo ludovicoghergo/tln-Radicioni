6d0c24d38d5021d149dd563732087ac1d1d10ed1
Ludah <ghergo.ludovico@educ.di.unito.it>
2021-09-20 20:22:16 +0200

    Disambiguazione con dictionary

--- Esercitazione_3/riassunto.py
+++ Esercitazione_3/riassunto.py
@@ -2,17 +2,18 @@ import re
 from nltk.corpus import wordnet as wn
 
 def disambiguazione (context, dup):
-    best_sense = dup[0][0]
+    best_sense = dup[0][2:]
     max_overlap = 0
     for current in dup:
         for l in Lines4:
-            if current[0] == l.split("\t")[0]:
-                signature = re.sub(r"[^a-zA-Z0-9]", "", l.split("\t")[1]).split()
+            pippo = l.split("\t")[0]
+            if current[0] == pippo:
+                signature = re.sub(r"[^a-zA-Z0-9 ]", "", l.split("\t")[1]).split()
                 signature = set(signature)
                 overlap = len(signature.intersection(context))
                 if overlap > max_overlap:
                     max_overlap = overlap
-                    best_sense = current
+                    best_sense = current[1:]
     return best_sense
 
 
@@ -27,15 +28,16 @@ def unify_vet (vettori):
     vet.sort(key= lambda x: float(x.split("_")[1]),reverse=True)
     return vet
 
-def create_context (frase, nasari, no_words):
+def create_context (frase):
     frase = re.sub(r"[^a-zA-Z0-9 ]", "", frase).lower().split(" ")
     frase = list(set(frase).difference(no_words))
+    pippo = dict_vet
     vettori = []
     for w in frase:
         dup_vet = []
-        for v in nasari:
-            if v[1].lower() == wn.morphy(w):
-                dup_vet.append(v)
+        if wn.morphy(w) in dict_vet:
+            for elem in dict_vet[wn.morphy(w)]:
+                dup_vet.append(elem)
         if (len(dup_vet)>1):
             vettori.append(disambiguazione(frase, dup_vet))
         elif (len(dup_vet)==1):
@@ -55,27 +57,32 @@ no_words = []
 for line in Lines3:
     no_words.append(line[:len(line)-1])
 no_words = set(no_words)
-vettori = []
+dict_vet = {}
 titolo = []
 i = 0
-
 for l in Lines2:
     elems = l.split(';')
     elems = list(filter(lambda a: a != '' and a !='\n', elems))
-    vettori.append(elems)
+    if(elems[1].lower() in dict_vet):
+        dict_vet[elems[1].lower()].append([elems[0]] + elems[2:])
+    else:
+        dict_vet[elems[1].lower()] = [[elems[0]] +elems[2:]]
 while len(titolo)<=0 :
     if (Lines[i][0] != '#' and Lines[i][0]!='\n'):
         titolo = Lines[i]
     i=i+1
-topic = create_context(titolo, vettori, no_words)
+topic = create_context(titolo)
 print(titolo)
 print(topic)
+
+val = 0
 
 paragraph = []
 for l in Lines:
     if (l[0] != '#' and l[0]!='\n'):
         paragraph.append(l)
 for p in paragraph:
-    p = [p, create_context(p, vettori, no_words)]
+    p = [p, create_context(p)]
     print(p)
+    val += 1
 
