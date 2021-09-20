ead1be25e0e1f8af1e95460c9a9ea210b4444233
mattia zito <mattia.zito@edu.unito.it>
2021-09-20 12:36:21 +0200

    esercitazione 3 - disambiguazione

--- Esercitazione_3/riassunto.py
+++ Esercitazione_3/riassunto.py
@@ -1,6 +1,21 @@
 import re
 from nltk.corpus import wordnet as wn
 
+def disambiguazione (context, dup):
+    best_sense = dup[0][0]
+    max_overlap = 0
+    for current in dup:
+        for l in Lines4:
+            if current[0] == l.split("\t")[0]:
+                signature = re.sub(r"[^a-zA-Z0-9]", "", l.split("\t")[1]).split()
+                signature = set(signature)
+                overlap = len(signature.intersection(context))
+                if overlap > max_overlap:
+                    max_overlap = overlap
+                    best_sense = current
+    return best_sense
+
+
 def unify_vet (vettori):
     vet = []
     for v in vettori:
@@ -17,9 +32,14 @@ def create_context (frase, nasari, no_words):
     frase = list(set(frase).difference(no_words))
     vettori = []
     for w in frase:
+        dup_vet = []
         for v in nasari:
             if v[1].lower() == wn.morphy(w):
-                vettori.append(v[2:])
+                dup_vet.append(v)
+        if (len(dup_vet)>1):
+            vettori.append(disambiguazione(frase, dup_vet))
+        elif (len(dup_vet)==1):
+            vettori.append(dup_vet[0][2:])
     return unify_vet(vettori)
 
 #leggo i file
@@ -29,6 +49,8 @@ file2 = open('utils/NASARI_vectors/dd-small-nasari-15.txt', encoding="utf8")
 Lines2 = file2.readlines()
 file3 = open('utils/stop_words_FULL.txt', 'r')
 Lines3 = file3.readlines()
+file4 = open('utils/babelnet.txt', encoding="utf8")
+Lines4 = file4.readlines()
 no_words = []
 for line in Lines3:
     no_words.append(line[:len(line)-1])
